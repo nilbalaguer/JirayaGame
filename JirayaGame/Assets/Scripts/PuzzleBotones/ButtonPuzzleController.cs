214ab99ba6dd0513a876f67db676dd4fb94a3e1b
nilbalaguer <nilbalaguerfp@ibf.cat>
2025-11-14 12:05:09 +0100

    mejora de finalizacion de puzzles

--- JirayaGame/Assets/Scripts/PuzzleBotones/ButtonPuzzleController.cs
+++ JirayaGame/Assets/Scripts/PuzzleBotones/ButtonPuzzleController.cs
@@ -10,15 +10,28 @@ public class ButtonPuzzleController : MonoBehaviour
 
     [SerializeField] GameObject partituraPrefab;
 
+    private bool exploded = false;
+
+    private ParticleSystem particleSystem;
+    private AudioSource audioSource;
+
+    private void Start() {
+        particleSystem = gameObject.GetComponentInChildren<ParticleSystem>();
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
     private void FixedUpdate() 
     {
         //El jugador tinen que acertar el numero 19
-        if (boton1.activado && !boton2.activado && !boton3.activado && boton4.activado && boton5.activado)
+        if (boton1.activado && !boton2.activado && !boton3.activado && boton4.activado && boton5.activado && !exploded)
         {
+            exploded = true;
             GameObject tempPartitura = Instantiate(partituraPrefab, transform.position, Quaternion.identity);
             PartituraItemScript tempPartituraItemScript = tempPartitura.GetComponent<PartituraItemScript>();
             tempPartituraItemScript.claveTPdesactivar = "mazzmorraBotones";
-            Destroy(gameObject);
+            particleSystem.Play();
+            audioSource.Play();
+            Destroy(gameObject, 2f);
         }
     }
 }

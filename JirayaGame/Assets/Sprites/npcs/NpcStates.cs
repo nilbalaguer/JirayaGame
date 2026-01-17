345fba253a967b69f7917e7d5f3fa4e840ad1994
Andreulinares <145133414+Andreulinares@users.noreply.github.com>
2026-01-17 19:31:06 +0100

    ajustes hud y nombres npc

--- JirayaGame/Assets/Sprites/npcs/NpcStates.cs
+++ JirayaGame/Assets/Sprites/npcs/NpcStates.cs
@@ -4,6 +4,7 @@ using TMPro;
 
 public class NpcStates : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
     public Rigidbody2D rb;
     private Animator anim;
 
@@ -56,6 +57,7 @@ public class NpcStates : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         currentState = State.Idle;
         
         /*if (NpcIntro)
@@ -219,7 +221,8 @@ public class NpcStates : MonoBehaviour
                 Vector2 directionToPlayer = (player.transform.position - transform.position).normalized;
                 if (Mathf.Abs(directionToPlayer.x) > Mathf.Abs(directionToPlayer.y))
                 {
-                    transform.localScale = new Vector3(directionToPlayer.x < 0 ? -3 : 3, 3, 3);
+                    //transform.localScale = new Vector3(directionToPlayer.x < 0 ? -3 : 3, 3, 3);
+                    spriteRenderer.flipX = directionToPlayer.x < 0;
                     anim.SetInteger("state", 7);
                 }
                 else
@@ -280,7 +283,7 @@ public class NpcStates : MonoBehaviour
                 directionToPlayer = (player.transform.position - transform.position).normalized;
                 if (Mathf.Abs(directionToPlayer.x) > Mathf.Abs(directionToPlayer.y))
                 {
-                    transform.localScale = new Vector3(directionToPlayer.x < 0 ? -3 : 3, 3, 3);
+                    spriteRenderer.flipX = directionToPlayer.x < 0;
                     anim.SetInteger("state", 7);
                 }
                 else
@@ -336,7 +339,8 @@ public class NpcStates : MonoBehaviour
             rb.linearVelocity = Vector2.zero;
             if (Mathf.Abs(directionToPlayer.x) > Mathf.Abs(directionToPlayer.y))
                 {
-                    transform.localScale = new Vector3(directionToPlayer.x < 0 ? -3 : 3, 3, 3);
+                    //transform.localScale = new Vector3(directionToPlayer.x < 0 ? -3 : 3, 3, 3);
+                    spriteRenderer.flipX = directionToPlayer.x < 0;
                     anim.SetInteger("state", 7);
                 }
                 else
@@ -379,7 +383,7 @@ public class NpcStates : MonoBehaviour
         if (absX > absY)
         {
             anim.SetInteger("state", 1);
-            transform.localScale = new Vector3(dir.x < 0 ? -3 : 3, 3, 3);
+            spriteRenderer.flipX = dir.x < 0;
         }
         else
         {
